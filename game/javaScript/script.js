4e824e0c3a554c05b4665e06f6c3da595baf1dd8
Laurina <laurinamadden@gmail.com>
2021-12-19 12:15:14 +0000

    Updated files

--- game/javaScript/script.js
+++ game/javaScript/script.js
@@ -339,3 +339,14 @@ function page3(){
 function page2(){
 	document.getElementById("L2").style.display = "block";
 }
+
+// jQuery
+$( "#hider" ).click(function() {
+  $( "span:last-child" ).hide( "fast", function() {
+    // Use arguments.callee so we don't need a named function
+    $( this ).prev().hide( "fast", arguments.callee );
+  });
+});
+$( "#shower" ).click(function() {
+  $( "span" ).show( 2000 );
+});
